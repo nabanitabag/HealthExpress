c7f3e7084b2168ca06720c973c1c8eb9dbfebad0
Nabanita <ashley.makruz@gmail.com>
2021-02-17 23:12:17 +0530

    add body parser and addition functionality

--- calculator.js
+++ calculator.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req, resp){
-  resp.send("Hello World");
+  resp.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000);
+app.post("/",function(req, resp){
+  console.log(req.body);
+  var result = Number(req.body.num1) + Number(req.body.num2);
+  resp.send("Thanks for posting. The result of your stupid numbers is " + result);
+});
+
+app.listen(3000, function(){
+  console.log("Server running");
+});
