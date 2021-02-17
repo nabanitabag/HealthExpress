c91027ff28694ce95f6a54a7bea5558a418292ef
Nabanita <ashley.makruz@gmail.com>
2021-02-17 23:51:23 +0530

    Add BMI calculator functionality.

--- calculator.js
+++ calculator.js
@@ -8,6 +8,17 @@ app.get("/",function(req, resp){
   resp.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmicalculator",function(req, resp){
+  resp.sendFile(__dirname + "/BMIcalculator.html");
+});
+
+app.post("/bmicalculator",function(req, resp){
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi =  weight/ (height *height);
+  resp.send("Your BMI is " + bmi +". Lose some weight!");
+});
+
 app.post("/",function(req, resp){
   console.log(req.body);
   var result = Number(req.body.num1) + Number(req.body.num2);
