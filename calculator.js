8e8a18b199f907972dcfebcaafe02c2f76263a38
Nabanita Bag <ashley.makruz@gmail.com>
2021-08-25 22:00:13 +0530

    add classes to bmi

--- calculator.js
+++ calculator.js
@@ -16,13 +16,28 @@ app.post("/bmicalculator",function(req, resp){
   var weight = parseFloat(req.body.weight);
   var height = parseFloat(req.body.height);
   var bmi =  weight/ (height *height);
-  resp.send("Your BMI is " + bmi +". Lose some weight!");
+  if(bmi <18.5)
+  {
+    resp.send("Your BMI is " + bmi +". You are probably underweight.");
+  }
+  else if(bmi >= 18.5 && bmi<=24.9)
+  {
+    resp.send("Your BMI is " + bmi +". You are probably normal.");
+  }
+  else if(bmi >= 25 && bmi<=29.9)
+  { 
+    resp.send("Your BMI is " + bmi +". You are probably overweight.");
+  }
+  else if(bmi >= 30)
+  { 
+    resp.send("Your BMI is " + bmi +". You are probably obese.");
+  }
 });
 
 app.post("/",function(req, resp){
   console.log(req.body);
   var result = Number(req.body.num1) + Number(req.body.num2);
-  resp.send("Thanks for posting. The result of your stupid numbers is " + result);
+  resp.send("Thanks for posting. The sum of your numbers is " + result);
 });
 
 app.listen(3000, function(){
